0a605344d61c3dc519c6063e9a3e4b31da3e97a1
Victor Leman <victorleman1@gmail.com>
2025-06-05 17:34:59 +0200

    remove dist folder from git tracking

--- dist/utils/functions.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.calculateCatchProbability = calculateCatchProbability;
-function calculateCatchProbability(fishLevel, catchRate) {
-    const diff = catchRate - fishLevel;
-    const probability = Math.min(Math.max(0.1 + diff / 100, 0.05), 0.95); // entre 5% et 95%
-    return probability;
-}
